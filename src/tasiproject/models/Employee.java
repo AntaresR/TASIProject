c949c683283fd25a58c118ae700de89985ea9536
Paolo <diagonal_zero@hotmail.com>
2016-09-28 11:28:39 -0500

    added some modes and the principle of the coding

---
+++ src/tasiproject/models/Employee.java
@@ -0,0 +1,182 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tasiproject.models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author paolo
+ */
+public class Employee {
+
+    private int id;
+    private String name;
+    private String lastname;
+    private String maternalLastName;
+    private Date birthday;
+    private String dni;
+    private String cellphone;
+    private String adress;
+    private Employ employ;
+    private Date entryDate;
+    private double cost;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the lastname
+     */
+    public String getLastname() {
+        return lastname;
+    }
+
+    /**
+     * @param lastname the lastname to set
+     */
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    /**
+     * @return the maternalLastName
+     */
+    public String getMaternalLastName() {
+        return maternalLastName;
+    }
+
+    /**
+     * @param maternalLastName the maternalLastName to set
+     */
+    public void setMaternalLastName(String maternalLastName) {
+        this.maternalLastName = maternalLastName;
+    }
+
+    /**
+     * @return the birthday
+     */
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    /**
+     * @param birthday the birthday to set
+     */
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    /**
+     * @return the dni
+     */
+    public String getDni() {
+        return dni;
+    }
+
+    /**
+     * @param dni the dni to set
+     */
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    /**
+     * @return the cellphone
+     */
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    /**
+     * @param cellphone the cellphone to set
+     */
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
+
+    /**
+     * @return the adress
+     */
+    public String getAdress() {
+        return adress;
+    }
+
+    /**
+     * @param adress the adress to set
+     */
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    /**
+     * @return the employ
+     */
+    public Employ getEmploy() {
+        return employ;
+    }
+
+    /**
+     * @param employ the employ to set
+     */
+    public void setEmploy(Employ employ) {
+        this.employ = employ;
+    }
+
+    /**
+     * @return the entryDate
+     */
+    public Date getEntryDate() {
+        return entryDate;
+    }
+
+    /**
+     * @param entryDate the entryDate to set
+     */
+    public void setEntryDate(Date entryDate) {
+        this.entryDate = entryDate;
+    }
+
+    /**
+     * @return the cost
+     */
+    public double getCost() {
+        return cost;
+    }
+
+    /**
+     * @param cost the cost to set
+     */
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
+ 
