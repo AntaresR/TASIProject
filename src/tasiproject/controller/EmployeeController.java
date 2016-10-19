db6c6a1c74dc121c517b2d5eb2f99b58ef342220
Paolo <diagonal_zero@hotmail.com>
2016-10-19 18:41:41 -0500

    some changes on validations

--- src/tasiproject/controller/EmployeeController.java
+++ src/tasiproject/controller/EmployeeController.java
@@ -15,12 +15,110 @@ public class EmployeeController {
 
     /**
      * Register the employee on the database
+     *
      * @param employee
      * @return operationOk the database signaled that everything went well
      */
-    public static boolean registerEmployee(Employee employee){
-        boolean operationOk = false;
+    public static boolean registerEmployee(Employee employee) {
+        boolean correct = false;
+
+        //Name
+        if (employee.getName() == null) {
+            return correct;
+        }
+        if (employee.getName().equals("")) {
+            return correct;
+        }
+        if (employee.getName().matches(".*\\d+.*")) {
+            return correct;
+        }
+
+        //Lastname
+        if (employee.getLastname() == null) {
+            return correct;
+        }
+
+        if (employee.getLastname().equals("")) {
+            return correct;
+        }
+        if (employee.getLastname().matches(".*\\d+.*")) {
+            return correct;
+        }
+
+        //Maternal Lastname
+        if (employee.getMaternalLastName() == null) {
+            return correct;
+        }
+
+        if (employee.getMaternalLastName().equals("")) {
+            return correct;
+        }
+        if (employee.getMaternalLastName().matches(".*\\d+.*")) {//if it is just numbers
+            return correct;
+        }
+
+        //DNI
+        if (employee.getDni() == null) {
+            return correct;
+        }
+
+        if (employee.getDni().equals("")) {
+            return correct;
+        }
+
+        if (!employee.getDni().matches(".*\\d+.*")) { //if is not just numbers
+            return correct;
+        }
+
+        //TODO Employ
+
+        // Begin date
+        if (employee.getEntryDate() == null) {
+            return correct;
+        }
+
+        // Salary
+        if (employee.getCost() > 10000) {
+            return correct;
+        }
+
+        if (employee.getCost() < 0) {
+            return correct;
+        }
+
+        // Address
+        if (employee.getBirthday() == null) {
+            return correct;
+        }
+
+        if (employee.getDni().equals("")) {
+            return correct;
+        }
+
+        if (employee.getDni().matches(".*\\d+.*")) { //if is just numbers
+            return correct;
+        }
+
+        // Birthday
+        if (employee.getBirthday() == null) {
+            return correct;
+        }
+
+        // Cellphone
+        if (employee.getCellphone()== null) {
+            return correct;
+        }
+
+        if (employee.getCellphone().equals("")) {
+            return correct;
+        }
+
+        if (!employee.getCellphone().matches(".*\\d+.*")) { //if is not just numbers
+            return correct;
+        }
+        
+        correct = true;
         //The employee is saved in the database
-        return operationOk;
-    } 
+        return correct;
+    }
 }
--- test/tasiproject/view/Employee_insert_test.java
+++ test/tasiproject/view/Employee_insert_test.java
@@ -13,12 +13,11 @@ import tasiproject.models.Employ;
 import tasiproject.models.Employee;
 
 /**
- *
- * @author alulab14
+ * @author paolo rossi ramayoni 20105435
  */
 public class Employee_insert_test {
 
-    //Prueba 1 : 1a2, 2b1, 3c1, 4d1, 5e1, 6f1, 7g1
+    //Prueba 1 : 1a1, 2b1, 3c1, 4d1, 5e1, 6f1, 7g1
     @Test
     public final void registerEmployee_correct_test() {
         //Variable initialization
@@ -29,7 +28,6 @@ public class Employee_insert_test {
         employee.setCost(10121);
         employee.setEntryDate(new Date());
         employee.setDni("java");
-        //
         employee.setAdress("Calle El Caserio Mz. F1 Lote 4");
         employee.setBirthday(new Date());
         employee.setCellphone("950078142");
@@ -52,7 +50,6 @@ public class Employee_insert_test {
         employee.setCost(10121);
         employee.setEntryDate(new Date());
         employee.setDni("java");
-        
         employee.setAdress("Calle El Caserio Mz. F1 Lote 4");
         employee.setBirthday(new Date());
         employee.setCellphone("950078142");
@@ -71,13 +68,18 @@ public class Employee_insert_test {
         Employee employee = new Employee();
         employee.setName("1233456");
         employee.setLastname("lastname");
-        employee.setLastnameM("lastnameM");
-        employee.setSalary(10121);
-        employee.setBegindate(new Date());
-        employee.setEmploy("Javero");
-        employee.setIdentityDocument("java");
-
-        Assert.assertEquals(EmployeeManager.registerEmployee(employee), false);
+        employee.setMaternalLastName("lastnameM");
+        employee.setCost(10121);
+        employee.setEntryDate(new Date());
+        employee.setDni("java");
+        employee.setAdress("Calle El Caserio Mz. F1 Lote 4");
+        employee.setBirthday(new Date());
+        employee.setCellphone("950078142"); 
+        //TODO for now is empty 
+        Employ employ = new Employ();
+        employee.setEmploy(employ);
+        
+        Assert.assertEquals(EmployeeController.registerEmployee(employee), false);
     }
 
     //Prueba 4 : 1a4, 2b1, 3c1, 4d1, 5e1, 6f1, 7g1
@@ -87,13 +89,18 @@ public class Employee_insert_test {
         Employee employee = new Employee();
         employee.setName(null);
         employee.setLastname("lastname");
-        employee.setLastnameM("lastnameM");
-        employee.setSalary(10121);
-        employee.setBegindate(new Date());
-        employee.setEmploy("Javero");
-        employee.setIdentityDocument("java");
+        employee.setMaternalLastName("lastnameM");
+        employee.setCost(10121);
+        employee.setEntryDate(new Date());
+        employee.setDni("java");
+        employee.setAdress("Calle El Caserio Mz. F1 Lote 4");
+        employee.setBirthday(new Date());
+        employee.setCellphone("950078142"); 
+        //TODO for now is empty 
+        Employ employ = new Employ();
+        employee.setEmploy(employ);
 
-        Assert.assertEquals(EmployeeManager.registerEmployee(employee), false);
+        Assert.assertEquals(EmployeeController.registerEmployee(employee), false);
     }
 
     //Prueba 5 : 1a1, 2b2, 3c1, 4d1, 5e1, 6f1, 7g1
@@ -103,13 +110,19 @@ public class Employee_insert_test {
         Employee employee = new Employee();
         employee.setName("Paolo");
         employee.setLastname("");
-        employee.setLastnameM("lastnameM");
-        employee.setSalary(10121);
-        employee.setBegindate(new Date());
-        employee.setEmploy("Javero");
-        employee.setIdentityDocument("java");
+        employee.setMaternalLastName("lastnameM");
+        employee.setCost(10121);
+        employee.setEntryDate(new Date());
+        employee.setDni("java");
+        
+        employee.setAdress("Calle El Caserio Mz. F1 Lote 4");
+        employee.setBirthday(new Date());
+        employee.setCellphone("950078142"); 
+        //TODO for now is empty 
+        Employ employ = new Employ();
+        employee.setEmploy(employ);
 
-        Assert.assertEquals(EmployeeManager.registerEmployee(employee), false);
+        Assert.assertEquals(EmployeeController.registerEmployee(employee), false);
     }
 
     //Prueba 6 : 1a1, 2b3, 3c1, 4d1, 5e1, 6f1, 7g1
@@ -119,13 +132,19 @@ public class Employee_insert_test {
         Employee employee = new Employee();
         employee.setName("Paolo");
         employee.setLastname("122345");
-        employee.setLastnameM("lastnameM");
-        employee.setSalary(10121);
-        employee.setBegindate(new Date());
-        employee.setEmploy("Javero");
-        employee.setIdentityDocument("java");
+        employee.setMaternalLastName("lastnameM");
+        employee.setCost(10121);
+        employee.setEntryDate(new Date());
+        employee.setDni("java");
+        
+        employee.setAdress("Calle El Caserio Mz. F1 Lote 4");
+        employee.setBirthday(new Date());
+        employee.setCellphone("950078142"); 
+        //TODO for now is empty 
+        Employ employ = new Employ();
+        employee.setEmploy(employ);
 
-        Assert.assertEquals(EmployeeManager.registerEmployee(employee), false);
+        Assert.assertEquals(EmployeeController.registerEmployee(employee), false);
     }
 
     //Prueba 7 : 1a1, 2b4, 3c1, 4d1, 5e1, 6f1, 7g1
@@ -135,13 +154,19 @@ public class Employee_insert_test {
         Employee employee = new Employee();
         employee.setName("Paolo");
         employee.setLastname(null);
-        employee.setLastnameM("lastnameM");
-        employee.setSalary(10121);
-        employee.setBegindate(new Date());
-        employee.setEmploy("Javero");
-        employee.setIdentityDocument("java");
+        employee.setMaternalLastName("lastnameM");
+        employee.setCost(10121);
+        employee.setEntryDate(new Date());
+        employee.setDni("java");
+        
+        employee.setAdress("Calle El Caserio Mz. F1 Lote 4");
+        employee.setBirthday(new Date());
+        employee.setCellphone("950078142"); 
+        //TODO for now is empty 
+        Employ employ = new Employ();
+        employee.setEmploy(employ);
 
-        Assert.assertEquals(EmployeeManager.registerEmployee(employee), false);
+        Assert.assertEquals(EmployeeController.registerEmployee(employee), false);
     }
 
     //Prueba 8 : 1a1, 2b1, 3c2, 4d1, 5e1, 6f1, 7g1
@@ -151,13 +176,19 @@ public class Employee_insert_test {
         Employee employee = new Employee();
         employee.setName("Alvkarito");
         employee.setLastname("lastname");
-        employee.setLastnameM("");
-        employee.setSalary(10121);
-        employee.setBegindate(new Date());
-        employee.setEmploy("Javero");
-        employee.setIdentityDocument("java");
+        employee.setMaternalLastName("");
+        employee.setCost(10121);
+        employee.setEntryDate(new Date());
+        employee.setDni("java");
 
-        Assert.assertEquals(EmployeeManager.registerEmployee(employee), false);
+        employee.setAdress("Calle El Caserio Mz. F1 Lote 4");
+        employee.setBirthday(new Date());
+        employee.setCellphone("950078142"); 
+        //TODO for now is empty 
+        Employ employ = new Employ();
+        employee.setEmploy(employ);
+
+        Assert.assertEquals(EmployeeController.registerEmployee(employee), false);
     }
     
     //Prueba 9 : 1a1, 2b1, 3c3, 4d1, 5e1, 6f1, 7g1
@@ -167,13 +198,19 @@ public class Employee_insert_test {
         Employee employee = new Employee();
         employee.setName(null);
         employee.setLastname("lastname");
-        employee.setLastnameM("lastnameM");
-        employee.setSalary(10121);
-        employee.setBegindate(new Date());
-        employee.setEmploy("Javero");
-        employee.setIdentityDocument("java");
+        employee.setMaternalLastName("lastnameM");
+        employee.setCost(10121);
+        employee.setEntryDate(new Date());
+        employee.setDni("java");
+        
+        employee.setAdress("Calle El Caserio Mz. F1 Lote 4");
+        employee.setBirthday(new Date());
+        employee.setCellphone("950078142"); 
+        //TODO for now is empty 
+        Employ employ = new Employ();
+        employee.setEmploy(employ);
 
-        Assert.assertEquals(EmployeeManager.registerEmployee(employee), false);
+        Assert.assertEquals(EmployeeController.registerEmployee(employee), false);
     }
      
     // Prueba 10 : 1a1, 2b1, 3c4, 4d1, 5e1, 6f1, 7g1
@@ -183,13 +220,19 @@ public class Employee_insert_test {
         Employee employee = new Employee();
         employee.setName(null);
         employee.setLastname("lastname");
-        employee.setLastnameM(null);
-        employee.setSalary(10121);
-        employee.setBegindate(new Date());
-        employee.setEmploy("Javero");
-        employee.setIdentityDocument("java");
+        employee.setMaternalLastName(null);
+        employee.setCost(10121);
+        employee.setEntryDate(new Date());
+        employee.setDni("java");
+        
+        employee.setAdress("Calle El Caserio Mz. F1 Lote 4");
+        employee.setBirthday(new Date());
+        employee.setCellphone("950078142"); 
+        //TODO for now is empty 
+        Employ employ = new Employ();
+        employee.setEmploy(employ);
 
-        Assert.assertEquals(EmployeeManager.registerEmployee(employee), false);
+        Assert.assertEquals(EmployeeController.registerEmployee(employee), false);
     }
     
      // Prueba 11 : 1a1, 2b1, 3c1, 4d2, 5e1, 6f1, 7g1
@@ -199,14 +242,78 @@ public class Employee_insert_test {
         Employee employee = new Employee();
         employee.setName("paolo");
         employee.setLastname("lastname");
-        employee.setLastnameM("paolo");
-        employee.setSalary(10121);
-        employee.setBegindate(new Date());
-        employee.setEmploy("Javero");
-        employee.setIdentityDocument("");
+        employee.setMaternalLastName("paolo");
+        employee.setCost(10121);
+        employee.setEntryDate(new Date());
+        employee.setDni("");
+        
+        employee.setAdress("Calle El Caserio Mz. F1 Lote 4");
+        employee.setBirthday(new Date());
+        employee.setCellphone("950078142"); 
+        //TODO for now is empty 
+        Employ employ = new Employ();
+        employee.setEmploy(employ);
 
-        Assert.assertEquals(EmployeeManager.registerEmployee(employee), false);
+        Assert.assertEquals(EmployeeController.registerEmployee(employee), false);
+    }   
+    
+    // Prueba 12 : 1a1, 2b1, 3c1, 4d3, 5e1, 6f1, 7g1, h1, i1, j1
+    @Test
+    public final void registerEmployee_alphanumerics_identityDocument_test() {
+        //Variable initialization
+        Employee employee = new Employee();
+        employee.setName("paolo");
+        employee.setLastname("lastname");
+        employee.setMaternalLastName("paolo");
+        employee.setCost(10121);
+        employee.setEntryDate(new Date());
+        employee.setDni("no hay doc");
+        
+        employee.setAdress("Calle El Caserio Mz. F1 Lote 4");
+        employee.setBirthday(new Date());
+        employee.setCellphone("950078142"); 
+        //TODO for now is empty 
+        Employ employ = new Employ();
+        employee.setEmploy(employ);
+
+        Assert.assertEquals(EmployeeController.registerEmployee(employee), false);
     }
+    
+    // Prueba 13 : 1a1, 2b1, 3c1, 4d4, 5e1, 6f1, 7g1, h1, i1, j1
+    
+    
+    // Prueba 14 : 1a1, 2b1, 3c1, 4d1, 5e2, 6f1, 7g1, h1, i1, j1
+    
+    
+    // Prueba 15 : 1a1, 2b1, 3c1, 4d1, 5e3, 6f1, 7g1, h1, i1, j1
+    
+    
+    // Prueba 16 : 1a1, 2b1, 3c1, 4d1, 5e4, 6f1, 7g1, h1, i1, j1
+    
+    
+    // Prueba 17 : 1a1, 2b1, 3c1, 4d1, 5e1, 6f2, 7g1, h1, i1, j1
+    
+    
+    // Prueba 18 : 1a1, 2b1, 3c1, 4d1, 5e1, 6f1, 7g2, h1, i1, j1
+    
+    
+    // Prueba 19 : 1a1, 2b1, 3c1, 4d1, 5e1, 6f1, 7g3, h1, i1, j1
+    
+    
+    // Prueba 20 : 1a1, 2b1, 3c1, 4d1, 5e1, 6f1, 7g3, h2, i1, j1
+    
+    
+    // Prueba 21 : 1a1, 2b1, 3c1, 4d1, 5e1, 6f1, 7g3, h3, i1, j1
+    
+    
+    // Prueba 22 : 1a1, 2b1, 3c1, 4d1, 5e1, 6f1, 7g3, h1, i2, j1
+    
+    
+    // Prueba 23 : 1a1, 2b1, 3c1, 4d1, 5e1, 6f1, 7g3, h1, i1, j2
+    
+    
+    // Prueba 24 : 1a1, 2b1, 3c1, 4d1, 5e1, 6f1, 7g3, h1, i1, j3
+    
     
     
 }
